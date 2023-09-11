e606d29aff4ccf41078a2d12096e75a47766b3af
Andy Zhang <67079259+azhang777@users.noreply.github.com>
2023-09-10 23:59:16 -0400

    first commit

--- project03/project02-03_txt.js
+++ project03/project02-03_txt.js
@@ -3,8 +3,39 @@
       Project 02-03
 
       Application to return the shape of a clicked object
-      Author: 
-      Date:   
+      Author: Andy Zhang
+      Date: 09/10/2023
 
       Filename: project02-03.js
  */
+//reference html elements
+const square = document.getElementById("square");
+const triangle = document.getElementById("triangle");
+const circle = document.getElementById("circle");
+const feedbackParagraph = document.getElementById("feedback");
+
+// Add mouseover event listeners to each shape div
+square.addEventListener("mouseover", () => {
+  feedbackParagraph.textContent = "You hovered over the square.";
+});
+
+triangle.addEventListener("mouseover", () => {
+  feedbackParagraph.textContent = "You hovered over the triangle.";
+});
+
+circle.addEventListener("mouseover", () => {
+  feedbackParagraph.textContent = "You hovered over the circle.";
+});
+
+// Add mouseout event listerns to each shape div
+square.addEventListener("mouseout", () => {
+  feedbackParagraph.textContent = "";
+});
+
+triangle.addEventListener("mouseout", () => {
+  feedbackParagraph.textContent = "";
+});
+
+circle.addEventListener("mouseout", () => {
+  feedbackParagraph.textContent = "";
+});
